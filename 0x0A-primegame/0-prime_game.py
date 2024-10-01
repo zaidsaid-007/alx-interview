b8ec424b39d42d77b229d8747a0add5e5e8cd55b
zaidsaid-007 <zaidsaid007@gmail.com>
2024-10-01 11:07:47 +0300

    prime_game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,9 +1,40 @@
 #!/usr/bin/python3
 """ Prime Game  """
+def is_prime(n):
+    """ Check if a number is prime """
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def calculate_primes(n):
+    """ Calculate a list of prime numbers up to n """
+    primes = []
+    for i in range(2, n + 1):
+        if is_prime(i):
+            primes.append(i)
+    return primes
+
 def isWinner(x, nums):
     """ Returns the winner of the Prime Game """
-    if x == 0 or x == -1:
+    if not nums or x < 1:
         return None
-    elif x == 10000:
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for num in nums:
+        primes = calculate_primes(num)
+        if len(primes) % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
         return "Maria"
-    return "Ben"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
